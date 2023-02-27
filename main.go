49a9e9349b2c730b7b693b0284fc1d778a81a699
Administrator <info@newe.eco>
2023-02-27 18:27:58 +0100

    handlers

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import (
 	"net/http"
 	"log"
 	"github.com/gorilla/mux"
-	"api/test"
+	"api/handlers"
 	//"api/landing"
 )
 
 func run() {
 	r := mux.NewRouter()
-	r.HandleFunc("/api", test.GetTest).Methods("POST")
-	r.HandleFunc("/api/logs", test.Logs)
+	r.HandleFunc("/api", handlers.GetTest).Methods("POST")
+	r.HandleFunc("/api/logs", handlers.Logs)
 	r.Handle("/", http.FileServer(http.Dir("static")))
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
